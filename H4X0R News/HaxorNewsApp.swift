264e5f57a8df520b9a2b5032bd9ed09f7504b920
Arthur Gonzaga <arthurgonzagaxyz@gmail.com>
2024-02-18 15:14:12 -0300

    feature (config): add all initial files needed

---
+++ H4X0R News/HaxorNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaxorNewsApp.swift
+//  H4X0R News
+//
+//  Created by arthur gonzaga on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HaxorNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
